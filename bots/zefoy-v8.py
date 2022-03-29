588b84404d7639f16544d4ece1e637238976060d
- X T E K K Y - <98614666+xtekky@users.noreply.github.com>
2022-03-29 15:20:05 +0200

    Create zefoy-v8.py

---
+++ bots/zefoy-v8.py
@@ -0,0 +1,38 @@
+print('''
+███████╗███████╗███████╗ ██████╗ ██╗   ██╗    ██████╗  ██████╗ ████████╗    ██╗   ██╗   █████╗ 
+╚══███╔╝██╔════╝██╔════╝██╔═══██╗╚██╗ ██╔╝    ██╔══██╗██╔═══██╗╚══██╔══╝    ██║   ██║  ██╔══██╗
+  ███╔╝ █████╗  █████╗  ██║   ██║ ╚████╔╝     ██████╔╝██║   ██║   ██║       ██║   ██║  ╚█████╔╝
+ ███╔╝  ██╔══╝  ██╔══╝  ██║   ██║  ╚██╔╝      ██╔══██╗██║   ██║   ██║       ╚██╗ ██╔╝  ██╔══██╗
+███████╗███████╗██║     ╚██████╔╝   ██║       ██████╔╝╚██████╔╝   ██║        ╚████╔╝██╗╚█████╔╝
+╚══════╝╚══════╝╚═╝      ╚═════╝    ╚═╝       ╚═════╝  ╚═════╝    ╚═╝         ╚═══╝ ╚═╝ ╚════╝ 
+A I - V E R S I O N
+
+SCRIPT IS AVAILABLE IN OUR DISCORD SERVER: https://discord.gg/m6wdhpMV5A
+JOIN OUR DISCORD TO GET V8
+
+---------------------------------------------------------------------------------------------------------------------------------------
+Z E F O Y - B O T  V.8
+How does zefoy bot v.8 work?
+The zefoy bot v.8 AI
+- Zefoy bot v.8 implements an Artificial Inteligence, that solves the captcha automatically.
+- This gives the bot the ability to keep running for an endless amount of time.
+Future versions and updates
+- Zefoy v.8 is a revolution, and in a very early phase.
+- As soon as you buy it, you unlock all futures updates.
+- For example: a hybrid zefoy bot that uses zefoy.com and freer.es.
+- at the same time, we are also currently coding a software, an .exe file.
+- Ve are also working on a cloud version, and web hosting.
+- Zefoy bot has a bright future you shouldn't miss out.
+Advantages
+- Once you buy the bot, you get acess to the future versions for FREE.
+- These future versions will of course be available to buy but more expensive.
+- You will also get the VIP role and acess to all future vip scripts.
+Pricing
+Zefoy bot is starting at very reasonable prices:
+- Script: 4.99€
+- Full Setup: 1.99€
+Want more?
+We have acess to our own botnet with awesome performances!
+- We offer Tiktok [followers/views/likes] and Instagram [followers/views/likes]
+- Aswell as many other options, DM us at xtekky#9031 and Anur#8475
+''')
