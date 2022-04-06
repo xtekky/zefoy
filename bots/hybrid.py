62b6b89959a32e90c7ee9bd0c145c5b4c5ea56ab
- X T E K K Y - <98614666+xtekky@users.noreply.github.com>
2022-04-06 03:24:08 +0200

    Create hybrid.py

---
+++ bots/hybrid.py
@@ -0,0 +1,31 @@
+import fade
+
+try:
+  print(fade.fire('''
+  ██░ ██▓██   ██▓ ▄▄▄▄    ██▀███   ██▓▓█████▄     ██▒   █▓ ██▓▓█████  █     █░  ██████ TM
+  ▓██░ ██▒▒██  ██▒▓█████▄ ▓██ ▒ ██▒▓██▒▒██▀ ██▌   ▓██░   █▒▓██▒▓█   ▀ ▓█░ █ ░█░▒██    ▒ 
+  ▒██▀▀██░ ▒██ ██░▒██▒ ▄██▓██ ░▄█ ▒▒██▒░██   █▌    ▓██  █▒░▒██▒▒███   ▒█░ █ ░█ ░ ▓██▄   
+  ░▓█ ░██  ░ ▐██▓░▒██░█▀  ▒██▀▀█▄  ░██░░▓█▄   ▌     ▒██ █░░░██░▒▓█  ▄ ░█░ █ ░█   ▒   ██▒
+  ░▓█▒░██▓ ░ ██▒▓░░▓█  ▀█▓░██▓ ▒██▒░██░░▒████▓       ▒▀█░  ░██░░▒████▒░░██▒██▓ ▒██████▒▒
+   ▒ ░░▒░▒  ██▒▒▒ ░▒▓███▀▒░ ▒▓ ░▒▓░░▓   ▒▒▓  ▒       ░ ▐░  ░▓  ░░ ▒░ ░░ ▓░▒ ▒  ▒ ▒▓▒ ▒ ░
+   ▒ ░▒░ ░▓██ ░▒░ ▒░▒   ░   ░▒ ░ ▒░ ▒ ░ ░ ▒  ▒       ░ ░░   ▒ ░ ░ ░  ░  ▒ ░ ░  ░ ░▒  ░ ░
+   ░  ░░ ░▒ ▒ ░░   ░    ░   ░░   ░  ▒ ░ ░ ░  ░           ░░   ▒ ░   ░     ░   ░  ░  ░  ░  
+   ░  ░  ░░ ░      ░         ░      ░     ░                        ░   ░     ░  ░    ░   
+          ░ ░           ░               ░               ░
+
+  SCRIPT IS AVAILABLE IN OUR DISCORD SERVER: https://discord.gg/onlp
+  JOIN OUR DISCORD TO GET VHYBRID AND ALL OTHER VERSIONS
+
+  > v8.3.3 - script with ai, fast => 30k/h (CAPTCHA ai)
+  > hybrid - zefoy.com + freer.es + more (in the future) - 45k/h
+
+  Price: 10.99€ / 11.99$ - premium subscription
+  > Benefits: - FREE acess to all future versions + BETA acess to public scrips + PREMIUM role in Discord server + AI keys acess
+  > Payment - PAYPAL, friends and family only (or won't work) + CYPTO btc/etg/mtc and more + SELLIX visit shop: onlpx.sellix.io
+  > Support - GET additional support for 1.99€ / 2.99$ (FULL setup + VIDEO chat)
+  '''))
+except ModuleNotFoundError:
+    print('''paste: 
+         pip install fade 
+in cmd - the module you tried to import was not found - make sure you have python installed befor proceeding''')
+    exit()
