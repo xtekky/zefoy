2c2e0ccbb669256e6f98c499fbfe2067c7492c95
- X T E K K Y - <98614666+xtekky@users.noreply.github.com>
2022-04-06 18:35:13 +0200

    Update hybrid.py

--- bots/hybrid.py
+++ bots/hybrid.py
@@ -24,9 +24,10 @@ in cmd - the module you tried to import was not found - make sure you have pytho
 
   > v8.3.3 - script with ai, fast => 30k/h (CAPTCHA ai)
   > hybrid - zefoy.com + freer.es + more (in the future) - 45k/h
-
+  > shop - https://onlp.sellix.io
+  
   Price: 10.99€ / 11.99$ - premium subscription
   > Benefits: - FREE acess to all future versions + BETA acess to public scrips + PREMIUM role in Discord server + AI keys acess
-  > Payment - PAYPAL, friends and family only (or won't work) + CYPTO btc/etg/mtc and more + SELLIX visit shop: onlpx.sellix.io
+  > Payment - PAYPAL, friends and family only (or won't work) + CYPTO btc/eth/ltc and more + SELLIX visit shop: onlp.sellix.io
   > Support - GET additional support for 1.99€ / 2.99$ (FULL setup + VIDEO chat)
   '''))
