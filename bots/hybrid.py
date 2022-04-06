ba6c80ad5b40052b91aa35247af65264b85da7ae
- X T E K K Y - <98614666+xtekky@users.noreply.github.com>
2022-04-06 10:36:47 +0200

    Update hybrid.py

--- bots/hybrid.py
+++ bots/hybrid.py
@@ -1,6 +1,12 @@
-import fade
-
 try:
+  import fade
+except ModuleNotFoundError:
+    print('''paste: 
+         pip install fade 
+in cmd - the module you tried to import was not found - make sure you have python installed befor proceeding''')
+    exit()
+
+
   print(fade.fire('''
   ██░ ██▓██   ██▓ ▄▄▄▄    ██▀███   ██▓▓█████▄     ██▒   █▓ ██▓▓█████  █     █░  ██████ TM
   ▓██░ ██▒▒██  ██▒▓█████▄ ▓██ ▒ ██▒▓██▒▒██▀ ██▌   ▓██░   █▒▓██▒▓█   ▀ ▓█░ █ ░█░▒██    ▒ 
@@ -24,8 +30,3 @@ try:
   > Payment - PAYPAL, friends and family only (or won't work) + CYPTO btc/etg/mtc and more + SELLIX visit shop: onlpx.sellix.io
   > Support - GET additional support for 1.99€ / 2.99$ (FULL setup + VIDEO chat)
   '''))
-except ModuleNotFoundError:
-    print('''paste: 
-         pip install fade 
-in cmd - the module you tried to import was not found - make sure you have python installed befor proceeding''')
-    exit()
