63635815b03d2ec00d70039bc6f8fdafb9d1bce3
XTEKKY <98614666+xtekky@users.noreply.github.com>
2022-04-26 14:54:36 +0200

    Create source.py

---
+++ source.py
@@ -0,0 +1,10 @@
+print('''
+
+Because of skids and assholes, I decided to remove the source, you can get the bot free in my server: discord.gg/onlp
+
+My only socials: Tekky#9999 | Tekky#6666 | discord.gg/onlp | discord.g/shares
+
+Made with <3 By Tekky
+
+I apologize for the inconvenience caused
+''')
