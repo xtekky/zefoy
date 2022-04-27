60a15c5ee692d1e727ec0516d48481808ec803e9
XTEKKY <98614666+xtekky@users.noreply.github.com>
2022-04-28 01:42:14 +0200

    ~

--- source.py
+++ source.py
@@ -2,7 +2,7 @@ print('''
 
 Because of skids and assholes, I decided to remove the source, you can get the bot free in my server: discord.gg/onlp
 
-My only socials: Tekky#9999 | Tekky#6666 | discord.gg/onlp | discord.g/shares
+My only socials: Tekky#9999 | discord.gg/onlp | discord.gg/shares
 
 Made with <3 By Tekky
 
