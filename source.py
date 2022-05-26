e879cc496cbbcc39e90fb9884036a02860f18c36
Tekky <98614666+xtekky@users.noreply.github.com>
2022-05-26 11:35:01 +0200

    Update source.py

--- source.py
+++ source.py
@@ -1,14 +1,5 @@
-print('''
-
-Because of skids and assholes, I decided to remove the source, you can get the bot free in my server: discord.gg/onlp
-
-My only socials: Tekky#9999 | discord.gg/onlp | discord.gg/shares
-
-Made with <3 By Tekky
-
-I apologize for the inconvenience caused, 
-
-github.com/xtekky
-''')
-
-input()
+input(
+  """
+  TikTok Bot is in discord.gg/onlp
+  """
+)
